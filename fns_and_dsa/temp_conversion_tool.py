2a25d19149e2e20891d526a94cd28eb7068a6853
Teboho Paul Mjikelo <paulmjikelo@gmail.com>
2025-09-28 19:41:38 +0200

    Add temp_conversion_tool.py with Celsius ↔ Fahrenheit conversion using global factors

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -6,7 +6,7 @@ def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR (9/5) + 32 #fahrenheit = (celsius * 9/5) + 32
 
 def main():
     try:
